74ebcb353e72a86c0420694910930017271ec028
Felipe Figueroa <amenadiel@gmail.com>
2014-10-10 17:18:29 -0300

    avances viernes 10

--- assets/js/craack.js
+++ assets/js/craack.js
@@ -1,3 +1,17 @@
+(function (i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function () {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-55640244-1', 'auto');
+ga('send', 'pageview');
+
 requirejs.config({
 
 	waitSeconds: 18,
@@ -84,6 +98,7 @@ require(['jquery',
 
 		},
 		initialize: function () {
+			this.bind('route', this._pageView);
 			var navmenuview = new navmenuView();
 			var navbarview = new navbarView();
 
@@ -91,6 +106,18 @@ require(['jquery',
 
 
 		},
+
+
+
+		_pageView: function () {
+			var path = Backbone.history.getFragment();
+			console.log('Send pageview ', "/" + path);
+			ga('send', 'pageview', {
+				page: "/" + path
+			});
+		},
+
+
 		home: function (hash) {
 			console.log('home', hash);
 			var landing = new LandingView();
